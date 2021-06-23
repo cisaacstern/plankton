c5c2a63a071f74610d26947af7982695a1331445
cisaacstern <62192187+cisaacstern@users.noreply.github.com>
2021-06-23 14:17:55 -0700

    first commit

---
+++ plankton.py
@@ -0,0 +1,15 @@
+
+PLANKTON_CONSTANT = 3.213456
+
+
+def calculate_flor(array):
+    """This function calculates fluoresence for a given input array.
+    """
+
+    return array * PLANKTON_CONSTANT
+
+
+class BinValues():
+    """I don't know what this is yet.
+    """
+    pass
